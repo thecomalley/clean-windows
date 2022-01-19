c6796af8d0ef598976eb7fcc38588887a0f58d0c
Chris O'Malley <31399219+thecomalley@users.noreply.github.com>
2022-01-19 13:00:16 +1300

    Create fresh.ps1

---
+++ fresh.ps1
@@ -0,0 +1,3 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1')); Get-Boxstarter -Force
+
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/thecomalley/clean-windows/main/boxstarter.txt -DisableReboots
